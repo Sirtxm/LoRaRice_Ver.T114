aff77868249f84ab9044001dc8b31683413cf880
Sirtxm <teeraphat.stamp@gmail.com>
2025-06-04 13:44:49 +0700

    Pump update

--- LoRaRice/Pump/Pump.ino
+++ LoRaRice/Pump/Pump.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <Adafruit_BME280>
+#include <Adafruit_BME280.h>
 #include <Wire.h>
 #include <BatteryMonitor.h>
 
@@ -7,7 +7,12 @@
 #define PIN_BAT_ADC     4
 #define PIN_BAT_ADC_CTL 6
 #define MY_BAT_AMPLIFY  4.9
-
+// ===== PIN LED =============
+#define LED_AUTO_ON     8
+#define LED_AUTO_OFF    7  
+#define RELAY_TRICK_ON  44
+#define RELAY_TRICK_OFF  46
+   
 // ===== Global Instances =====
 TwoWire *wi = &Wire;
 Adafruit_BME280 bme;
@@ -15,17 +20,23 @@ BatteryMonitor battery(PIN_BAT_ADC, PIN_BAT_ADC_CTL, MY_BAT_AMPLIFY);
 
 // ===== State Machine =====
 enum {
-  READ_SENSOR = 0;
-  LORA_MODE   = 1;
-  AUTO_MODE   = 2;
-  MANUAL_MODE = 3;
-}
+  READ_SENSOR = 0,
+  LORA_MODE   = 1,
+  ON_PUMP   = 2,
+  OFF_PUMP  = 3
+};
 
 int state = READ_SENSOR;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
   Serial.println("Booting Mesh Node T114...");
+
+  pinMode(LED_AUTO_ON, OUTPUT);
+  pinMode(LED_AUTO_OFF, OUTPUT);
+  pinMode(RELAY_TRICK_ON, OUTPUT);
+  pinMode(RELAY_TRICK_OFF, OUTPUT);
 
   // ===== Init I2C Bus =====
   Serial.println("Setting up I2C...");
@@ -66,11 +77,21 @@ void loop() {
   else if (state == LORA_MODE){
     
   }
-  else if (state == AUTO_MODE){
-    
+  else if (state == ON_PUMP){
+    digitalWrite(LED_AUTO_ON, HIGH);
+    digitalWrite(RELAY_TRICK_ON, HIGH);
+    delay(1000);
+    digitalWrite(LED_AUTO_ON, LOW);
+    digitalWrite(RELAY_TRICK_ON, LOW);
     state = READ_SENSOR;
   }
-  else if (state == MANUAL_MODE){
+  else if (state == OFF_PUMP){
+    digitalWrite(LED_AUTO_OFF, HIGH);
+    digitalWrite(RELAY_TRICK_OFF, HIGH);
+    delay(1000);
+    digitalWrite(LED_AUTO_OFF, LOW);
+    digitalWrite(RELAY_TRICK_OFF, LOW);
     state = READ_SENSOR;
   }
+
 }
