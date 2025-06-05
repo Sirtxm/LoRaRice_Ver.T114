23b32f40a6ab1746e3cce99d02778728e6c66e84
Sirtxm <teeraphat.stamp@gmail.com>
2025-06-05 10:06:05 +0700

    Update

--- LoRaRice/Pump/Pump.ino
+++ LoRaRice/Pump/Pump.ino
@@ -10,16 +10,24 @@ uint8_t devEui[] = { 0x22, 0x32, 0x33, 0x00, 0x00, 0x99, 0xaa, 0x04 };
 uint8_t appEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xF0, 0x00, 0x00, 0x00 };
 uint8_t appKey[] = { 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88 };
 
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88 };
+uint8_t appSKey[] = { 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88 };
+uint32_t devAddr = (uint32_t)0x007e6ae4;
+
 // ===== LoRaWAN Settings =====
-LoRaMacRegion_t loraWanRegion = LORAMAC_REGION_AS923;
+LoRaMacRegion_t loraWanRegion = LORAMAC_REGION_AS923;  
 DeviceClass_t loraWanClass = CLASS_A;
 bool overTheAirActivation = true;
 bool loraWanAdr = true;
 bool isTxConfirmed = false;
 uint8_t appPort = 2;
 uint8_t confirmedNbTrials = 1;
-uint32_t appTxDutyCycle = 30000;
-#define APP_TX_DUTYCYCLE_RND 500
+uint32_t appTxDutyCycle = 15000;
+
+uint16_t userChannelsMask[6] = { 0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
+
+#define APP_TX_DUTYCYCLE_RND 1000 
 
 // ===== Pin Definitions =====
 #define PIN_BAT_ADC     4
@@ -36,7 +44,13 @@ Adafruit_BME280 bme;
 BatteryMonitor battery(PIN_BAT_ADC, PIN_BAT_ADC_CTL, MY_BAT_AMPLIFY);
 
 // ===== Pump Control Variables =====
-bool pumpOnCommand = false;   // รับค่าจาก Downlink
+enum PumpState{
+  STATE_PUMP_AUTO = 0,
+  STATE_PUMP_MANUAL = 1
+};
+PumpState pumpState;
+uint8_t pumpCmd;
+
 
 // ===== LoraWAN Payload =====
 void prepareTxFrame(uint8_t port) {
@@ -54,39 +68,57 @@ void prepareTxFrame(uint8_t port) {
   appData[5] = batt & 0xFF;
 }
 
+// ===== Pump Control Logic =====
+void controlPump(PumpState state, uint8_t cmd) {
+  if (state == STATE_PUMP_AUTO && cmd == 0x01) {
+    digitalWrite(LED_AUTO_ON, LOW);
+    digitalWrite(RELAY_TRICK_ON, LOW);
+    delay(1000);
+    digitalWrite(LED_AUTO_ON, HIGH);
+    digitalWrite(RELAY_TRICK_ON, HIGH);
+  } 
+  else if(state == STATE_PUMP_AUTO && cmd == 0x00){
+    digitalWrite(LED_AUTO_OFF, LOW);
+    digitalWrite(RELAY_TRICK_OFF, LOW);
+    delay(1000);
+    digitalWrite(LED_AUTO_OFF, HIGH);
+    digitalWrite(RELAY_TRICK_OFF, HIGH);
+  }
+  else if(state == STATE_PUMP_MANUAL && cmd == 0x01){
+    digitalWrite(RELAY_TRICK_ON, LOW);
+    delay(1000);
+    digitalWrite(RELAY_TRICK_ON, HIGH);
+  }
+  else if(state == STATE_PUMP_MANUAL && cmd == 0x00){
+    digitalWrite(RELAY_TRICK_OFF, LOW);
+    delay(1000);
+    digitalWrite(RELAY_TRICK_OFF, HIGH);
+  }
+}
+
 // ===== Downlink Control =====
 void downLinkDataHandle(McpsIndication_t *mcpsIndication)
 {
-  if(mcpsIndication->BufferSize >= 1) {
-    uint8_t cmd = mcpsIndication->Buffer[0];
-    switch(cmd) {
+  if(mcpsIndication->BufferSize >= 2) {
+    uint8_t mode = mcpsIndication->Buffer[0];
+    uint8_t cmd = mcpsIndication->Buffer[1];
+    switch(mode) {
       case 0x00:
-        Serial.println("Command: Turn PUMP OFF");
-        pumpOnCommand = false;
+        Serial.println("Mode: Auto Level Trigger");
+        pumpState = STATE_PUMP_AUTO;
+        pumpCmd = cmd;
+        controlPump(pumpState, pumpCmd);
         break;
       case 0x01:
-        Serial.println("Command: Turn PUMP ON");
-        pumpOnCommand = true;
+        Serial.println("Mode: Remote manual");
+        pumpState = STATE_PUMP_MANUAL;
+        pumpCmd = cmd;
+        controlPump(pumpState, pumpCmd);
         break;
       default:
         Serial.printf("Unknown downlink command: 0x%02X\n", cmd);
         break;
     }
-  }
-}
-
-// ===== Pump Control Logic =====
-void controlPump() {
-  if (pumpOnCommand) {
-    digitalWrite(LED_AUTO_ON, HIGH);
-    digitalWrite(RELAY_TRICK_ON, HIGH);
-    digitalWrite(LED_AUTO_OFF, LOW);
-    digitalWrite(RELAY_TRICK_OFF, LOW);
-  } else {
-    digitalWrite(LED_AUTO_OFF, HIGH);
-    digitalWrite(RELAY_TRICK_OFF, HIGH);
-    digitalWrite(LED_AUTO_ON, LOW);
-    digitalWrite(RELAY_TRICK_ON, LOW);
   }
 }
 
@@ -125,7 +157,7 @@ void loop() {
     case DEVICE_STATE_SEND:
       prepareTxFrame(appPort);
       LoRaWAN.send();
-      controlPump();  // สั่งปั๊มทุกครั้งหลังส่ง
+      controlPump(pumpState, pumpCmd);  
       deviceState = DEVICE_STATE_CYCLE;
       break;
 
